3f9a749cfe7ce7b02850fe10678a4a7b932678fd
jchen790 <jchen790@gmail.com>
2018-04-01 01:19:41 -0400

    creating components

--- script.js
+++ script.js
@@ -1,4 +1,60 @@
-var app = new Vue({
+// All the components
+Vue.component('home', {
+    template: `<p>
+                    Home page
+                </p>`
+})
+
+Vue.component('upcoming-events', {
+    template: `<p>
+                    upcoming events page
+                </p>`
+})
+
+Vue.component('past-events', {
+    template: `<p>
+                    past events page
+                </p>`
+})
+
+Vue.component('event-details', {
+    template: `<p>
+                    new events/event details page
+                </p>`
+})
+
+Vue.component('navbar', {
+    template: `<p>
+                    Navbar
+                </p>`
+})
+
+Vue.component('button', {
+    template: `<p>
+                    button
+                </p>`
+})
+
+Vue.component('table', {
+    template: `<p>
+                    table
+                </p>`
+})
+
+Vue.component('progress-bar', {
+    template: `<p>
+                    progress bar
+                </p>`
+})
+
+Vue.component('event-section', {
+    template: `<p>
+                    event section
+                </p>`
+})
+
+// Initialize Vue
+new Vue({
     el: '#app',
     data: {
       message: 'Hello Vue!'
