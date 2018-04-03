462475c832da828c317ec609fbea58053acd13e0
jchen790 <jchen790@gmail.com>
2018-04-03 13:35:29 -0400

    collapsible working

--- script.js
+++ script.js
@@ -1,37 +1,4 @@
 // All the components and associated variables
-Vue.component('tab-home', {
-    template: `<div class="page">
-                    <h1>PlanIt</h1>
-                    <img src="./logo2.png" class="home-img">
-                    <p>
-                        PlanIt is the only event-planning app you'll need. Here, you'll be able to see 
-                        everything that goes into event planning and be able to track the progress of every task.
-                        In a world of event planning tasks, all you need to do is visit PlanIt!
-                    </p>
-                    <div>
-                    <button type="button" class="new-event btn btn-light">New Event</button>
-                    </div>
-                    <div>
-                        <button type="button" class="btn btn-secondary"> Upcoming Events</button>
-                        <button type="button" class="btn btn-secondary">Past Events</button>
-                    </div>
-                </div>`
-})
-
-Vue.component('tab-upcoming', {
-    template: `<div class="page">
-                    <h1>Upcoming Events</h1>
-                    <event-table></event-table>
-                </div>`
-})
-
-Vue.component('tab-past', {
-    template: `<div class="page">
-                    <h1>Past Events</h1>
-                    <event-table></event-table>
-                </div>`
-})
-
 Vue.component('tab-details', {
     data: function () {
         return {
