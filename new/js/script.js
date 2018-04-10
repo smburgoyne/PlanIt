91030bcbab609df7980557c93d1c009a9cb4b454
jchen790 <jchen790@gmail.com>
2018-04-10 02:31:44 -0400

    fixed form props

--- new/js/script.js
+++ new/js/script.js
@@ -93,23 +93,41 @@ function enableAllInputs()
     $('#name').prop('disabled', false);
     $('#date').prop('disabled', false);
     $('#time').prop('disabled', false);
+    $('#location-dropdown-button').prop('disabled', false);
     $('#place').prop('disabled', false);
     $('#about').prop('disabled', false);
     $('#planner').prop('disabled', false);
     $('#submit1').prop('disabled', false);
     $('#cancel1').prop('disabled', false);
 
-    // enable finance buttons
+    $('#budget-dropdown-button').prop('disabled', false);
+    $('#request-budget-button').prop('disabled', false);
+    $('#supplies-list-button').prop('disabled', false);
+    $('#list-link').prop('disabled', false);
+    $('#request-reimbursement-button').prop('disabled', false);
+    $('#submit2').prop('disabled', false);
+    $('#cancel2').prop('disabled', false);
 
     $('#date2').prop('disabled', false);
+    $('#location-dropdown-button1').prop('disabled', false);
     $('#place2').prop('disabled', false);
     $('#time2').prop('disabled', false);
-    // enable other logistics buttons
+    $('#supplies-list-button1').prop('disabled', false);
+    $('#list-link2').prop('disabled', false);
+    $('#signin-form-button').prop('disabled', false);
+    $('#signin-link').prop('disabled', false);
+    $('#volunteer-form-button').prop('disabled', false);
+    $('#volunteer-link').prop('disabled', false);
+    $('#carpool-form-button').prop('disabled', false);
+    $('#carpool-link').prop('disabled', false);
     $('#submit3').prop('disabled', false);
     $('#cancel3').prop('disabled', false);
 
-    // enable marketing buttons
+    $('#request-ad-button').prop('disabled', false);
+    $('#submit4').prop('disabled', false);
+    $('#cancel4').prop('disabled', false);
 
+    $('#feedback-form-button').prop('disabled', false);
     $('#notes').prop('disabled', false);
     $('#submit5').prop('disabled', false);
     $('#cancel5').prop('disabled', false);
@@ -132,6 +150,11 @@ function resetNewEventPage()
     $('#planner').val("");
     $('#progressbar > div').css('width', '0%');
     completion = 0;
+    $('#list-link').val("");
+    $('#list-link2').val("");
+    $('#signin-link').val("");
+    $('#volunteer-link').val("");
+    $('#carpool-link').val("");
 
     $('#detail-title').text("Create New Event");
     $('#location-type').text("Where is the event happening?");
@@ -146,27 +169,45 @@ function resetNewEventPage()
     $('#name').prop('disabled', false);
     $('#date').prop('disabled', false);
     $('#time').prop('disabled', false);
+    $('#location-dropdown-button').prop('disabled', false);
     $('#place').prop('disabled', false);
     $('#about').prop('disabled', false);
     $('#planner').prop('disabled', false);
     $('#submit1').prop('disabled', false);
     $('#cancel1').prop('disabled', false);
 
-    // disable finance buttons
+    $('#budget-dropdown-button').prop('disabled', true);
+    $('#request-budget-button').prop('disabled', true);
+    $('#supplies-list-button').prop('disabled', true);
+    $('#list-link').prop('disabled', true);
+    $('#request-reimbursement-button').prop('disabled', true);
+    $('#submit2').prop('disabled', true);
+    $('#cancel2').prop('disabled', true);
 
     $('#date2').prop('disabled', true);
+    $('#location-dropdown-button1').prop('disabled', true);
     $('#place2').prop('disabled', true);
     $('#time2').prop('disabled', true);
-    // disable other logistics buttons
+    $('#supplies-list-button1').prop('disabled', true);
+    $('#list-link2').prop('disabled', true);
+    $('#signin-form-button').prop('disabled', true);
+    $('#signin-link').prop('disabled', true);
+    $('#volunteer-form-button').prop('disabled', true);
+    $('#volunteer-link').prop('disabled', true);
+    $('#carpool-form-button').prop('disabled', true);
+    $('#carpool-link').prop('disabled', true);
     $('#submit3').prop('disabled', true);
     $('#cancel3').prop('disabled', true);
 
-    // disable marketing buttons
+    $('#request-ad-button').prop('disabled', true);
+    $('#submit4').prop('disabled', true);
+    $('#cancel4').prop('disabled', true);
 
+    $('#feedback-form-button').prop('disabled', true);
     $('#notes').prop('disabled', true);
     $('#submit5').prop('disabled', true);
     $('#cancel5').prop('disabled', true);
-
+    
     $('#submit').prop('disabled', true);
 }
 
@@ -176,26 +217,44 @@ function disableAllInputs()
     $('#name').prop('disabled', true);
     $('#date').prop('disabled', true);
     $('#time').prop('disabled', true);
+    $('#location-dropdown-button').prop('disabled', true);
     $('#place').prop('disabled', true);
     $('#about').prop('disabled', true);
     $('#planner').prop('disabled', true);
     $('#submit1').prop('disabled', true);
     $('#cancel1').prop('disabled', true);
 
-    // disable finance buttons
+    $('#budget-dropdown-button').prop('disabled', true);
+    $('#request-budget-button').prop('disabled', true);
+    $('#supplies-list-button').prop('disabled', true);
+    $('#list-link').prop('disabled', true);
+    $('#request-reimbursement-button').prop('disabled', true);
+    $('#submit2').prop('disabled', true);
+    $('#cancel2').prop('disabled', true);
 
     $('#date2').prop('disabled', true);
+    $('#location-dropdown-button1').prop('disabled', true);
     $('#place2').prop('disabled', true);
     $('#time2').prop('disabled', true);
-    // disable other logistics buttons
+    $('#supplies-list-button1').prop('disabled', true);
+    $('#list-link2').prop('disabled', true);
+    $('#signin-form-button').prop('disabled', true);
+    $('#signin-link').prop('disabled', true);
+    $('#volunteer-form-button').prop('disabled', true);
+    $('#volunteer-link').prop('disabled', true);
+    $('#carpool-form-button').prop('disabled', true);
+    $('#carpool-link').prop('disabled', true);
     $('#submit3').prop('disabled', true);
     $('#cancel3').prop('disabled', true);
 
-    // disable marketing buttons
+    $('#request-ad-button').prop('disabled', true);
+    $('#submit4').prop('disabled', true);
+    $('#cancel4').prop('disabled', true);
 
+    $('#feedback-form-button').prop('disabled', true);
     $('#notes').prop('disabled', true);
     $('#submit5').prop('disabled', true);
     $('#cancel5').prop('disabled', true);
-
+    
     $('#submit').prop('disabled', true);
 }
