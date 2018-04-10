2085013db48033460e76f0e36bce30a717bc05ef
jchen790 <jchen790@gmail.com>
2018-04-10 07:55:07 -0400

    badge working

--- new/js/notifications.js
+++ new/js/notifications.js
@@ -5,6 +5,7 @@
 */
 
 $(document).ready(function () {
+    var noteCount = 0;
     var dbref = firebase.database().ref('/Organizations/' + currentOrg + "/Events/");
     dbref.on('value', snap => {
         var notificationsTable = $("#notifications-table");
@@ -23,6 +24,7 @@ $(document).ready(function () {
                     // note - establish budget
                     addNotification("Is your event free? Remember to complete the Finances section for " + eventName);
                     console.log("note - establish budget");
+                    noteCount += 1;
                 }
 
                 if(child.child("IsFree").val() == "No")
@@ -32,12 +34,14 @@ $(document).ready(function () {
                         // note - request a budget
                         addNotification("Remember to request a budget from your Treasurer for " + eventName);
                         console.log("note - request budget");
+                        noteCount += 1;
                     }
                     if(child.child('BudgetStatus').val() == "Pending")
                     {
                         // note - check with treasurer
                         addNotification("We see that you have requested a budget for " + eventName + ". Make sure to follow up with your Treasurer!");
                         console.log("note - check with treasurer");
+                        noteCount += 1;
                     }
                 }
 
@@ -49,12 +53,14 @@ $(document).ready(function () {
                     // note - choose a location
                     addNotification("Where is your event? Remember to choose a location for " + eventName);
                     console.log("note - choose location");
+                    noteCount += 1;
                 }
                 if(child.child('Location').val() == "On" || child.child('Location').val() == "Off")
                 {
                     // note - enter an address
                     addNotification("Do you have an address for your event? Make sure to enter an address for " + eventName);
                     console.log("note - enter address");
+                    noteCount += 1;
                 }
 
                 // check for forms
@@ -66,6 +72,7 @@ $(document).ready(function () {
                     //note - courtesy reminder of these forms
                     addNotification("Just a friendly reminder that " + eventName + " is in 1 week, so make sure to create sign-in, volunteer, and carpool forms!");
                     console.log("note - friendly reminder to create forms");
+                    noteCount += 1;
                 }
 
                 // check for ad
@@ -74,12 +81,14 @@ $(document).ready(function () {
                     // note - request for advertising
                     addNotification("Have you planned how to market your event? Remember to request advertising for " + eventName);
                     console.log("note - request advertising");
+                    noteCount += 1;
                 }
                 if(child.child('AdStatus') == "Pending" && (eventTimeDiff < 14 && eventTimeDiff >= 0))
                 {
                     // note - check with advertiser
                     addNotification("We see that you've requested to advertise " + eventName + ". Make sure to follow up with your Advertising Chair!");
                     console.log("note - check with advertiser");
+                    noteCount += 1;
                 }
 
                 // post notifications
@@ -88,15 +97,18 @@ $(document).ready(function () {
                     // note - reminder to complete event
                     addNotification("Remember to fill out feedback forms and any reimbursement forms for " + eventName + ". Also, remember to complete the event!");
                     console.log("note - complete event");
+                    noteCount += 1;
                 }
                 if(eventTimeDiff >= -7 && eventTimeDiff < -2)
                 {
                     // note - final reminder
                     addNotification("Here is your final reminder to fill out feedback forms and any reimbursement forms for" + eventName + ". Also, remember to complete the event!");
                     console.log("note - (final) complete event");
+                    noteCount += 1;
                 }
             }
         });
+        $('#badge').text(noteCount);
     });
 });
 
